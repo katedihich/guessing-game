1d073afe25b1a2c1f07c02aa128232c654850905
Kate <k.dihich@gmail.com>
2024-07-17 12:33:02 +0100

    high score add

--- script.js
+++ script.js
@@ -11,8 +11,9 @@
 const findSecretNum = () => {
   return Math.trunc(Math.random() * 20) + 1;
 };
-const secretNum = findSecretNum();
-document.querySelector('.number').textContent = secretNum;
+let secretNum = findSecretNum();
+let score = 20;
+let highScore = 0;
 
 // Establish message
 const displayMessage = message => {
@@ -23,7 +24,7 @@ document
   .querySelector('.check')
   .addEventListener('click', function eventHandler() {
     const guess = Number(document.querySelector('.guess').value);
-    console.log(guess, typeof guess);
+    //console.log(guess, typeof guess);
 
     if (!guess) {
       displayMessage('No number!');
@@ -33,11 +34,27 @@ document
       displayMessage('You win!');
       document.querySelector('body').style.backgroundColor = '#60b347';
       document.querySelector('.number').textContent = secretNum;
+      if (score > highScore) {
+        highScore = score;
+        document.querySelector('.highscore').textContent = highScore;
+      }
     } else if (guess !== secretNum) {
       guess < secretNum
         ? displayMessage('Too low')
         : displayMessage('Too high');
-    } else if (guess > 20) {
-      displayMessage('Between 1 and 20!');
+      score--;
+      document.querySelector('.score').textContent = score;
     }
+  });
+
+document
+  .querySelector('.btn.again')
+  .addEventListener('click', function eventHandler() {
+    displayMessage('Start guessing...');
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.guess').value = '';
+    document.querySelector('.number').textContent = '?';
+    secretNum = findSecretNum(); // Generate a new secret number
+    score = 20;
+    document.querySelector('.score').textContent = score;
   });
